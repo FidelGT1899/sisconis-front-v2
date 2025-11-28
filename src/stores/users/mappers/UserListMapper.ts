3e99bb4a373219da4cb385274d71302650dfdbaa
FidelGT <87815887+FidelGT1899@users.noreply.github.com>
2025-11-28 13:28:55 -0500

    fix: reduce eslint restriction (#5)

--- src/stores/users/mappers/UserListMapper.ts
+++ src/stores/users/mappers/UserListMapper.ts
@@ -1,31 +1,29 @@
-import type { User } from '@/modules/users/domain/entities/User'
-import type { Role } from '@/modules/users/domain/entities/Role'
-import type { Area } from '@/modules/users/domain/entities/Area'
-import type { Department } from '@/modules/users/domain/entities/Department'
-import type { UserListItemDTO } from '../dtos/userListItemDTO'
+import type { User } from '@/modules/users/domain/entities/User';
+import type { Role } from '@/modules/users/domain/entities/Role';
+import type { Area } from '@/modules/users/domain/entities/Area';
+import type { Department } from '@/modules/users/domain/entities/Department';
+import type { UserListItemDTO } from '../dtos/UserListItemDTO';
 
 export class UserListMapper {
-    static map(
-        users: User[],
-        roles: Role[],
-        areas: Area[],
-        departments: Department[]
-    ): UserListItemDTO[] {
-        return users.map(u => {
-            const area = areas.find(a => a.id === u.areaId)
-            const role = roles.find(r => r.id === u.roleId)
-            const dept = area
-                ? departments.find(d => d.id === area.departmentId)
-                : undefined
+  static map(
+    users: User[],
+    roles: Role[],
+    areas: Area[],
+    departments: Department[],
+  ): UserListItemDTO[] {
+    return users.map((u) => {
+      const area = areas.find((a) => a.id === u.areaId);
+      const role = roles.find((r) => r.id === u.roleId);
+      const dept = area ? departments.find((d) => d.id === area.departmentId) : undefined;
 
-            return {
-                id: u.id,
-                fullName: `${u.name} ${u.lastname}`,
-                email: u.email,
-                roleName: role?.name ?? 'Sin rol',
-                areaName: area?.name ?? 'Sin área',
-                departmentName: dept?.name ?? 'Sin departamento',
-            }
-        })
-    }
+      return {
+        id: u.id,
+        fullName: `${u.name} ${u.lastname}`,
+        email: u.email,
+        roleName: role?.name ?? 'Sin rol',
+        areaName: area?.name ?? 'Sin área',
+        departmentName: dept?.name ?? 'Sin departamento',
+      };
+    });
+  }
 }
